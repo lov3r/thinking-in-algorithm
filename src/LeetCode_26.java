a578c43696e4e563eb7d88644cf4f883e39bfcba
lov3r <gigabytc@163.com>
2019-11-08 22:13:30 +0800

    LeetCode的26题：增加双指针法

--- src/LeetCode_26.java
+++ src/LeetCode_26.java
@@ -5,28 +5,45 @@
  */
 
 public class LeetCode_26 {
-  private static int removeDuplicates(int[] nums) {
-    if(nums.length==0){
-      return 0;
-    }
-    //记录不重复的值
-    int j=nums[0];
-    //记录重复值的总数
-    int k=0;
-    for (int i = 1; i < nums.length; i++) {
-      if(nums[i]==j){
-        k++;
-      }else{
-        //移动替换
-        nums[i-k]=nums[i];
-        j=nums[i];
+
+  /**
+   * 双指针法
+   * @param nums
+   * @return
+   */
+  public static int removeDuplicates(int[] nums) {
+    if (nums.length == 0) return 0;
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+      if (nums[j] != nums[i]) {
+        i++;
+        nums[i] = nums[j];
       }
     }
-    return nums.length-k;
+    return i + 1;
   }
+//  private static int removeDuplicates(int[] nums) {
+//    if(nums.length==0){
+//      return 0;
+//    }
+//    //记录不重复的值
+//    int j=nums[0];
+//    //记录重复值的总数
+//    int k=0;
+//    for (int i = 1; i < nums.length; i++) {
+//      if(nums[i]==j){
+//        k++;
+//      }else{
+//        //移动替换
+//        nums[i-k]=nums[i];
+//        j=nums[i];
+//      }
+//    }
+//    return nums.length-k;
+//  }
 
   public static void main(String[] args) {
-    int[] nums={1,1,2};
+    int[] nums={0,0,1,1,1,2,2,3,3,4};
     int i=removeDuplicates(nums);
     System.out.println(i);
   }
